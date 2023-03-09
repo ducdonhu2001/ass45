1fe4acbb3a1d5432144cc79560658a30086f7791
ducdonhu 2001 <ducdonhu001@gmail.com>
2023-03-09 12:21:02 +0700

    set up

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecomstore',  
         'USER': 'root',  
-        'PASSWORD': '22022001',  
+        'PASSWORD': '123456789',  
         'HOST': '127.0.0.1',  
         'PORT': '3306', 
     }
